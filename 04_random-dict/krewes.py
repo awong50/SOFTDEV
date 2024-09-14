34a8cd19a9b72d8aec5178104c134ce57ee14ef4
Aidan <aidanwonglam@gmail.com>
2024-09-14 09:54:30 -0400

    Created krewes.py with heading and dictionary

---
+++ 04_random-dict/krewes.py
@@ -0,0 +1,23 @@
+"""
+Aidan Wong
+Elmos_Cheez-it
+SoftDev
+K04 -- Finding and returning random element in dictionary
+2024-
+time spent: 
+"""
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+			],
+           5: [ 
+		'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+		'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+		'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+		'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+			]
+        }
