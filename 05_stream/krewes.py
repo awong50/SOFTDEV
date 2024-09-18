eae161e98291b9c8ee170936b465bdb9c1908f32
Aidan <aidanwonglam@gmail.com>
2024-09-17 22:51:59 -0400

    Completed code to select random devo and print their information

--- 05_stream/krewes.py
+++ 05_stream/krewes.py
@@ -2,15 +2,34 @@
 Aidan Wong
 Elmos_Cheez-it
 SoftDev
-05_bitsream
+K05 -- Bitstream
 2024-09-17
 time spent: 0.5
 """
+
+import random
 
 f = open("krewes.txt", "r")
 people = f.read().replace("\n", "").split('@@@')
 for count, person in enumerate(people):
     people[count] = person.split('$$$')
-    person = {people[count][0], people[count][1], people[count][2]}
+    people[count] = {"period":people[count][0], "devo":people[count][1], "ducky":people[count][2]}
 
+randoDevo = random.randint(0, len(people)-1)
+
+print("Name: " + people[randoDevo].get("devo") + " | Period: " + people[randoDevo].get("period") + " | Ducky: " + people[randoDevo].get("ducky"))
+
+
+""""
 print(people)
+
+#Lets say you want to know all of the people/duckys in period 4
+p4Peeps = []
+p4Duckys = []
+for person in people:
+    if person.get("period") == '4':
+        p4Peeps.append(person.get("devo"))
+        p4Duckys.append(person.get("ducky"))
+print(p4Peeps)
+print(p4Duckys)
+"""
