225d5b6d705bacedf96adf5c76972de4514aed1b
Aidan Wong <awong50@stuy.edu>
2024-09-18 10:44:19 -0400

    Changed to use larger krewess.txt file with more names

--- 05_stream/krewes.py
+++ 05_stream/krewes.py
@@ -6,11 +6,11 @@ K05 -- Bitstream
 2024-09-17
 time spent: 0.5
 """
-
 import random
 
-f = open("krewes.txt", "r")
+f = open("krewess.txt", "r")
 people = f.read().replace("\n", "").split('@@@')
+people = people[:len(people)-1]
 for count, person in enumerate(people):
     people[count] = person.split('$$$')
     people[count] = {"period":people[count][0], "devo":people[count][1], "ducky":people[count][2]}
@@ -19,15 +19,14 @@ randoDevo = random.randint(0, len(people)-1)
 
 print("Name: " + people[randoDevo].get("devo") + " | Period: " + people[randoDevo].get("period") + " | Ducky: " + people[randoDevo].get("ducky"))
 
-
-""""
+"""
 print(people)
 
 #Lets say you want to know all of the people/duckys in period 4
 p4Peeps = []
 p4Duckys = []
 for person in people:
-    if person.get("period") == '4':
+    if person.get("period") == '05':
         p4Peeps.append(person.get("devo"))
         p4Duckys.append(person.get("ducky"))
 print(p4Peeps)
