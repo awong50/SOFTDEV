3ef24477831d2ba6f88853aba0c1dd2c5449da50
Aidan <aidanwonglam@gmail.com>
2024-09-19 23:50:52 -0400

    Completed weighted random from csv file with comments and heading

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -4,26 +4,55 @@ Mac_N_Cheez-its
 SoftDev
 K06 - Reading CSV files
 2024-09-19
-time spent:
+time spent: 1.0
 
-DISCO:
+DISCO: Keys of dict = Job class; Use int() to convert % into integers; use .split(",") to seperate values in table
 
-QCC:
+QCC: First and last items don't go into dictionary; Commas appear in quotations (use those to split); How to randomly select by weight
 
-HOW THIS SCRIPT WORKS:
+HOW THIS SCRIPT WORKS: Read occupations.csv and assign it to "data", stripping it and splitting it by new line; Iterate through "data" and replace commas inside quotations with "<>" to preserve them; Iterate through "data" again, splitting it by commas to get the key and value (the key being the occupation name and the value being the weight), after splitting replace "<>" with commas and quotes with empty strings, finally adding it to the dictionary and appending the weight value to a weights list, Randomly select random occupation (either with random.choices() or custom method below), print random occupation
 """
 
 import random
 
-occupationDict = {}
-f = open("occupations.csv", "r")
-occupations = f.read().strip().split("\n")
-for _ in range(1, len(occupations) - 1):
-    job = occupations[_]
-    if job[0] == '""':
-        key = job[1:].index('""')
-        tmp = job[0:key].replace(",", "<>") + job[key:]
-        occupations[_] = tmp
+with open("occupations.csv") as file: # Open file with this method so it closes by itself
+    data = file.read().strip().split("\n")
 
-print(occupations)
-    
+occupation = {}
+weights = []
+
+for i in range(1, len(data)-1):
+    job = data[i]
+    if job[0]=='"': # Look for quotations and replace commas with placeholder inside them
+        key = job[1:].index('"')
+        temp = job[0:key].replace(",","<>") + job[key:] # replacing necessary comma with placeholder
+        data[i] = temp # Assign the value with the placeholder to data
+
+for i in range(1, len(data)-1):
+    splitted = data[i].split(",") # Split by comma (reason why commas were replaced within the quotations before -- in order to preserve and not split those commas)
+    splitted[0] = splitted[0].replace("<>", ",").replace('"',"") # replacing placeholder with comma and removing the quotes
+    occupation[splitted[0]] = splitted[1] # Add to dictionary
+    weights.append(float(splitted[1])) # Append weight value to weight list
+
+# Logic to select a random occupation with weights: Iterate through weights list and keeps track of a culmative value. If adding a specific weight causes it to exceed the randomly generated value, it will print that as the random occupation
+for i in range (0, 1000): # Inside a for loop to do many testings quickly
+    total_weight = sum(weights)
+    culmative_weight = 0          
+    randomVal = random.uniform(0, total_weight) # random.uniform to include float numbers
+    randO = None # Assigned to None for no value
+
+    for i, weight in enumerate(weights): # For loop that checks with the culmative value
+        culmative_weight += weight 
+        if randomVal <= culmative_weight:
+            randO = list(occupation.keys())[i] # Turns the occupations.keys() object to a list to select the ith value from it (as the weights list and dictionary key values are aligned -- occupation corresponds to weight)
+            break # Break to prevent the for loop to continue running and always select the last key in dictionary
+    print(randO)
+ 
+
+"""" 
+Method using random.choices
+
+randO = random.choices(list(occupation), weights=weight, k=500)
+
+print(randO)
+"""   
