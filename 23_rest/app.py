3f8b19261552913992b5e2dc46045f0ad67d9b8e
Aidan <aidanwonglam@gmail.com>
2024-11-20 20:30:55 -0500

    Completed image on website generation

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -3,18 +3,28 @@ HallsRelief: Aidan Wong, Brian Liu, Victor Casado
 SoftDev
 K23 - Introduction to APIs
 2024-10-20
-time spent: 0.3
+time spent: 0.6
 """
 
-
 from flask import Flask, render_template
+import os, json
+import urllib.request
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+# Get the key from the key_nasa.txt
+with open("key_nasa.txt", "r") as key_file:
+    api_key = key_file.read().strip()
 
-@app.route("/")
+@app.route('/')
 def main():
-    return render_template('main.html')
+    with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=" + api_key) as response:
+        html = response.read() # Read API response
+        str = html.decode('utf-8') # Decode the bytes into string
+        data = json.loads(str) # Converts JSON string to python library
+        # print(data) # Prints the dictionary with the data
+    return render_template("main.html", explanation=data["explanation"], url=data["hdurl"])
 
-if __name__ == "__main__":
+if __name__ == "__main__": # false if this file imported as module
     app.debug = True
-    app.run()
+    app.run(port=5001)
