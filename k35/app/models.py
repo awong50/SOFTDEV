15ea636b551b3d2f23e1c4c8583428616c9322e0
Aidan <aidanwonglam@gmail.com>
2025-03-12 20:35:43 -0400

    Completed skeletal ai code project

--- k35/app/models.py
+++ k35/app/models.py
@@ -1,24 +1,46 @@
-from app import db
-from flask_login import UserMixin
-from datetime import datetime
+"""
+JAVA: Aidan Wong, Vedant Kothari
+SoftDev
+K35: Now Again for the First Time
+2024-03-12
+Time Spent: 2
+"""
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    contributions = db.relationship('Contribution', backref='author', lazy=True)
+class User:
+    users = []
 
-class Story(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    latest_update = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    contributions = db.relationship('Contribution', backref='story', lazy=True)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-class Contribution(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    story_id = db.Column(db.Integer, db.ForeignKey('story.id'), nullable=False
+    @classmethod
+    def create_user(cls, username, password):
+        new_user = cls(username, password)
+        cls.users.append(new_user)
+        return new_user
+
+    @classmethod
+    def find_user(cls, username):
+        for user in cls.users:
+            if user.username == username:
+                return user
+        return None
+
+
+class Story:
+    stories = []
+
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.contributions = []
+
+    @classmethod
+    def create_story(cls, title, content, author):
+        new_story = cls(title, content, author)
+        cls.stories.append(new_story)
+        return new_story
+
+    def add_contribution(self, contribution):
+        self.contributions.append(contribution)
